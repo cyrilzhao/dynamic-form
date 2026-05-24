9021a2ebb3fd1e3f47024111a18693ed310602ec
cyrilzhao <cyrilzhao@qq.com>
2026-05-24 23:29:14 +0800

    feat: 增加类型导出

--- src/components/DynamicForm/index.ts
+++ src/components/DynamicForm/index.ts
@@ -2,3 +2,5 @@ export { DynamicForm } from './DynamicForm';
 export type { DynamicFormProps, DynamicFormRef, FieldWidgetProps } from './types';
 export { FieldRegistry } from './core/FieldRegistry';
 export { SchemaParser } from './core/SchemaParser';
+export type { ExtendedJSONSchema, FieldOption } from './types/schema';
+export type { LinkageConfig, LinkageFunction, LinkageFunctionContext, ConditionExpression, ConditionOperator, LinkageEffect, LinkageType } from './types/linkage';
